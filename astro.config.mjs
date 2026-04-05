9ce5c6d463d27e9d5dea422d9a76cc6492ca504a
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-04-05 02:01:38 -0300

    feat: :construction: init dashboard page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
+import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://rifate.app",
+	site: "https://rifate.app",
+  integrations: [icon()],
   vite: {
     plugins: [tailwindcss()]
   },
+
   fonts: [{
     provider: fontProviders.fontsource(),
     name: "Nunito",
-    cssVariable: "--fontsource-nunito"
-  }]
+    cssVariable: "--fontsource-nunito",
+    weights: [400, 500, 600, 700, 800, 900]
+  }],
 });
