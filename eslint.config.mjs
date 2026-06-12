adfab4346bf3032902ae3f262673425f4c49d824
Mauro Daniel viveros <maurod.viveros@gmail.com>
2026-06-12 12:40:28 -0300

    chore: :label: add import sorting and unused imports ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,9 @@
 import typescriptParser from '@typescript-eslint/parser';
 import eslintPluginAstro from 'eslint-plugin-astro';
 import eslintPluginPrettier from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tseslint from 'typescript-eslint';
+import unusedImports from 'eslint-plugin-unused-imports';
 
 export default [
   { ignores: ['dist/', '.astro/', 'node_modules/'] },
@@ -46,6 +48,19 @@ export default [
     },
     rules: {
       'prettier/prettier': 'error',
+    },
+  },
+
+  // Imports: orden + detectar imports sin uso
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'unused-imports/no-unused-imports': 'error',
     },
   },
 
